ea4e817a7e83cffe048ca36ec7ecaa1482ac8bdc
admssen <wisenorwegian@gmail.com>
2025-04-06 13:11:02 +0500

    ni

--- authentify.sh
+++ authentify.sh
@@ -3,7 +3,8 @@ SEARCH="$(ls /var/www/filesystem/subjects | grep $1)"
 if [ "$SEARCH" = "$1" ]; then
 	VERIFY="$(cat /var/www/filesystem/subjects/$1)"
 	if [ "$VERIFY" = "$2" ]; then
-		echo "1"
+		ROLES="$(grep $1 /var/www/filesystem/roles)"
+		echo "1\n${ROLES}"
 	else
 		echo "0"
 	fi
