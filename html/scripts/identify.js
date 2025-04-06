08a61bc1ad05ec2c33b45dffef8558a30b4bfef6
admssen <wisenorwegian@gmail.com>
2025-04-06 11:34:09 +0500

    ni

--- html/scripts/identify.js
+++ html/scripts/identify.js
@@ -1,5 +1,5 @@
 function identify() {
-    var name = "name="+document.getElementById('name').value;
+    var name = document.getElementById('name').value;
     console.log(name);
     var q = new XMLHttpRequest();
     q.open('POST', 'scripts/identify.php', true);
