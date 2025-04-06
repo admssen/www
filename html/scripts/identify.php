6e8d475dc8a63fcc8748b3626a191826c038d003
admssen <wisenorwegian@gmail.com>
2025-04-06 11:26:04 +0500

    ni

--- html/scripts/identify.php
+++ html/scripts/identify.php
@@ -2,7 +2,7 @@
 
 $name = $_POST["name"];
 identify($name);
-$reply = shell_exec("bash /var/www/identify.sh" . $name . "2>&1");
+$reply = shell_exec("bash /var/www/identify.sh " . $name . " 2>&1");
 echo $reply;
 
 ?>
