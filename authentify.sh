de2071ab97660ddfc7e2697b2a120c7c933a2715
admssen <wisenorwegian@gmail.com>
2025-04-06 13:12:06 +0500

    ni

--- authentify.sh
+++ authentify.sh
@@ -3,7 +3,7 @@ SEARCH="$(ls /var/www/filesystem/subjects | grep $1)"
 if [ "$SEARCH" = "$1" ]; then
 	VERIFY="$(cat /var/www/filesystem/subjects/$1)"
 	if [ "$VERIFY" = "$2" ]; then
-		ROLES="$(grep $1 /var/www/filesystem/roles)"
+		ROLES="$(grep $1 /var/www/filesystem/roles/*)"
 		echo "1\n${ROLES}"
 	else
 		echo "0"
