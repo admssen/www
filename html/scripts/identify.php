8d64e2e14b9045e426095567eae965d4af16fb6d
admssen <wisenorwegian@gmail.com>
2025-04-06 11:32:16 +0500

    ni

--- html/scripts/identify.php
+++ html/scripts/identify.php
@@ -1,7 +1,7 @@
 <?php
 
 $name = $_POST["name"];
-$reply = shell_exec("ls /var/www/filesystem/subjects 2>&1");
+$reply = shell_exec("ls /var/www/filesystem/subjects | grep".$name."2>&1");
 echo $reply;
 
 ?>
