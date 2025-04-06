8c10901be394910551c384d357f53082719560c4
admssen <wisenorwegian@gmail.com>
2025-04-06 12:50:22 +0500

    ni

--- authentify.sh
+++ authentify.sh
@@ -3,7 +3,7 @@ SEARCH="$(ls /var/www/filesystem/subjects | grep $1)"
 if [ "$SEARCH" = "$1" ]; then
 	VERIFY="$(cat /var/www/filesystem/subjects/$1)"
 	if [ "$VERIFY" = "$2" ]; then
-		echo $2
+		echo "1"
 	else
 		echo "0"
 	fi
