78066df5d8bf6d0ed8e03438d9743876dc7bfc37
admssen <wisenorwegian@gmail.com>
2025-04-06 12:12:07 +0500

    ni

--- html/scripts/identify.js
+++ html/scripts/identify.js
@@ -1,10 +1,10 @@
 function identify() {
     var name = document.getElementById('name').value;
-    console.log(name);
     var q = new XMLHttpRequest();
     q.open('POST', 'scripts/identify.php', true);
     q.onload = function() {
         let stat = this.responseText;
+        console.log(stat);
         if (stat=='1') {
             console.log('success');
             document.getElementById('identified').innerHTML="IDENTIFICATION: SUCCESSFUL";
