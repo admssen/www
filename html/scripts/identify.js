57fca20c23641de3fa911f446ad026bdca982d7c
admssen <wisenorwegian@gmail.com>
2025-04-06 12:17:44 +0500

    ni

--- html/scripts/identify.js
+++ html/scripts/identify.js
@@ -4,12 +4,10 @@ function identify() {
     q.open('POST', 'scripts/identify.php', true);
     q.onload = function() {
         let stat = this.responseText;
-        console.log(stat);
         if (stat==1) {
-            console.log('success');
             document.getElementById('identified').innerHTML="IDENTIFICATION: SUCCESSFUL";
-        }else {
-            document.getElementById('identified').innetHTML="IDENTIFICATION: FAILURE";
+        } else {
+            document.getElementById('identified').innerHTML="IDENTIFICATION: FAILURE";
         }
     }
     q.onerror = function() {
