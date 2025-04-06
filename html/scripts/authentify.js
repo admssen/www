c79e5dbbd98074a7f66e94701675d6713bae13f4
admssen <wisenorwegian@gmail.com>
2025-04-06 15:28:39 +0500

    ni

--- html/scripts/authentify.js
+++ html/scripts/authentify.js
@@ -25,7 +25,7 @@ function authentify() {
 
 function displayRoles(role){
     role=role.split(':')[0];
-    role.replaceAll('_', ' ');
+    role.replaceAll("_", " ");
     const box = document.getElementById('rolelist');
     tmp = document.createElement('div');
     tmp.classList.add('role');
