cc5fde2289a92049816a6a18a9ebe8671d23cb50
admssen <wisenorwegian@gmail.com>
2025-04-06 16:19:15 +0500

    ni

--- html/scripts/authentify.js
+++ html/scripts/authentify.js
@@ -12,7 +12,7 @@ function authentify() {
             document.getElementById('ver').innerHTML = "CHANGE ACCOUNT";
             document.getElementById('authstat').innerHTML = "AUTHORIZED AS";
             document.getElementById('identified').innerHTML = name;
-            document.getElementById('dir').style.monWidth = "322px";
+            document.getElementById('dir').style.minWidth = "322px";
             document.getElementById('dir').style.backgroundColor = "#666688";
             var roles = stat.split(/\r\n|\r|\n/);
             roles = roles.slice(1, -1);
