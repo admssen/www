c20f5f76ba91b30debfda6943e0abe1bc0f0235a
admssen <wisenorwegian@gmail.com>
2025-04-08 07:39:39 +0500

    ni

--- html/scripts/authorize.php
+++ html/scripts/authorize.php
@@ -2,7 +2,7 @@
 
 $destination = $_GET["destination"];
 $token = $_GET["token"];
-$reply = shell_exec("bash /var/www/authorize.sh ".$destination." ".$token." 2>&1");
+$reply = shell_exec("bash /var/www/authorize.sh ".$destination." 2>&1");
 echo $reply;
 
 ?>
