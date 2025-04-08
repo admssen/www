51d7ec13425a5c8cb6da2c6ca0c027de5877ebd8
admssen <wisenorwegian@gmail.com>
2025-04-08 05:50:45 +0500

    ni

--- authentify.sh
+++ authentify.sh
@@ -5,7 +5,10 @@ if [ "$SEARCH" = "$1" ]; then
 	if [ "$VERIFY" = "$2" ]; then
 		cd /var/www/filesystem/roles/
 		ROLES="$(grep $1 *)"
+		cd /var/www/
+		TOKEN="$(./token_gen)"
 		echo "1
+${TOKEN}
 ${ROLES}"
 	else
 		echo "0"
