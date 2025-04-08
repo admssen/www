c4f88cdb3a6ff97868c740399f3c4d2913b2a3db
admssen <wisenorwegian@gmail.com>
2025-04-08 10:46:32 +0500

    ahh

--- html/scripts/authentify.js
+++ html/scripts/authentify.js
@@ -79,6 +79,10 @@ function authorizeAccess(offer){
         let stat = this.responseText;
         console.log(stat);
         if (stat[0]=="1"){
+            var items = stat.split(/\r\n|\r|\n/);
+            items = items.slice(1, -1);
+            document.getElementById('navi').innerHTML = "";
+            items.forEach((offer) => offerMenu(offer));
             document.getElementById('locate').innerHTML="success";
         } else {
             document.getElementById('locate').innerHTML="DENIED";
