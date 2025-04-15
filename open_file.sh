dd5a5e397323beb2c926b612b014dae37ad59fbd
admssen <wisenorwegian@gmail.com>
2025-04-15 12:23:29 +0500

    goofy ass

--- open_file.sh
+++ open_file.sh
@@ -3,8 +3,10 @@ cd /var/www
 PATH="/var/www/filesystem/"$5"/.access/"$3"/"$2
 if [ -e $PATH ]; then
 	ASSIGNED="menuitem"
+	APATH=$PATH
 else	
 	ASSIGNED="none"
+	APATH="/var/www/filesystem/"$5"/.access/directory"
 fi
 ALLOW="$(./token_verify $1 $2 $3 $ASSIGNED $5)"
 echo "$ALLOW"
@@ -12,5 +14,7 @@ if [ "$ALLOW" = "1" ]; then
 	GO="/var/www/filesystem/"$5
 	echo $GO
 	cd /bin
+	./cat $PATH
+	echo "_content_start_"
 	./cat $GO"/"$2
 fi
