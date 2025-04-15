a03e6ce52868269381390a6c082fc1716b2f6c46
admssen <wisenorwegian@gmail.com>
2025-04-15 11:09:57 +0500

    goofy ass

--- open_file.sh
+++ open_file.sh
@@ -2,14 +2,12 @@
 cd /var/www
 PATH="var/www/filesystem/roles/.access/"+$3+"/"+$2
 if [ -e $PATH ]; then
-	echo "yes"
 	ASSIGNED="menuitem"
 else	
-	echo "no"
 	ASSIGNED="none"
 fi
 ALLOW="$(./token_verify $1 $2 $3 $ASSIGNED $5)"
 echo "$ALLOW"
 if [ "$ALLOW" = "1" ]; then
-	echo "yes"
+	cat $PATH
 fi
