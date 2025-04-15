9a6431ae8640d4ac4919e6232e41d7fb2ce4cb64
admssen <wisenorwegian@gmail.com>
2025-04-15 12:00:16 +0500

    goofy ass

--- html/scripts/authentify.js
+++ html/scripts/authentify.js
@@ -185,7 +185,7 @@ function readFile(offer, isdir) {
             var disp="";
             items.forEach((lne) => disp=disp+"\n"+lne);
             document.getElementById('locate').innerHTML=location;
-            document.getElementById('pad').value="nigger";
+            document.getElementById('pad').value=disp;
         } else {
             document.getElementById('locate').style.color="red";
         }
